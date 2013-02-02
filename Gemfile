961ba192991ebbd21d19725cb4615baee664dd48
Justin Love <git@JustinLove.name>
2013-02-02 10:11:39 -0600

    mostly plain rails new

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+
+gem 'sidekiq'
+gem 'autoscaler'
+
+group :development do
+  gem 'foreman'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoscalerSample::Application.config.secret_token = '9ae151def4baa160e074c4d473dad9d9e684b800e76ce594a61b9fd4d8c164f589708dd207944800eb91e360b2dccf8422dae950d4239905e218f2b3cf1f8e00'
