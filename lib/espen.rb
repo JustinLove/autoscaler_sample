b5087aff9c5b62076337526005a72c036c2e0c0d
Justin Love <git@JustinLove.name>
2013-04-26 14:34:44 -0500

    espen config

---
+++ lib/espen.rb
@@ -0,0 +1,18 @@
+require 'sidekiq'
+require 'autoscaler/sidekiq'
+require 'autoscaler/heroku_scaler'
+heroku = Autoscaler::HerokuScaler.new
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+  config.client_middleware do |chain|
+    chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
+  end
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 4 }
+  config.server_middleware do |chain|
+    chain.add(Autoscaler::Sidekiq::Server, heroku, 60)
+  end
+end
