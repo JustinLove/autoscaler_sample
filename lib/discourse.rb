cf650ff72e2d3292b79a16413b9645f4c6f8906a
Justin Love <git@JustinLove.name>
2013-06-03 14:33:34 -0500

    discourse configuration

---
+++ lib/discourse.rb
@@ -0,0 +1,27 @@
+sidekiq_redis = { url: ENV['REDIS_URL'] || ENV['REDISTOGO_URL'], namespace: 'sidekiq' }
+
+if Rails.env.production? 
+
+  require 'autoscaler/sidekiq'
+  require 'autoscaler/heroku_scaler'
+
+    Sidekiq.configure_server do |config| 
+      config.redis = sidekiq_redis
+      config.server_middleware do |chain|
+        chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new('sidekiq'), 60)
+      end
+    end
+
+    Sidekiq.configure_client do |config| 
+      config.redis = sidekiq_redis
+      config.client_middleware do |chain|
+        chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new('sidekiq')
+      end
+    end
+
+else
+
+  Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
+  Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
+
+end
